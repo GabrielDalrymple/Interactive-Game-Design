fe97b170dce021698f724b77d0985ef511f9daae
Gabriel Smith-Dalrymple <Gabriel.Dalrymple@gmail.com>
2020-11-23 01:37:31 -0800

    Updates and clean up

---
+++ Team Project/Aquaroom Submission/Aquaroom/Assets/Scripts/AssetButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetButton : MonoBehaviour
+{
+    [SerializeField] Asset assetPrefab;
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<AssetButton>();
+        foreach (AssetButton button in buttons) {
+            button.GetComponent<SpriteRenderer>().color = new Color32(126, 126, 126, 255);
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+        FindObjectOfType<AssetSpawner>().SetSelectedAsset(assetPrefab);
+    }
+}
---
+++ Team Project/Aquaroom Submission/Aquaroom/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0) { StartCoroutine(WaitForTime()); }
+    }
+    IEnumerator WaitForTime() {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadStartScreen() {
+        WaitForTime();
+    }
+    public void LoadNextScene() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void StartOver() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void RestartFist() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void CloseGame() {
+        Application.Quit();
+    }
+}
